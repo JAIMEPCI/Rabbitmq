4ab6fc753018533d2977b1e6349e7f67ddf35b15
JAIME P.C.I <jaimeolarte888@gmail.com>
2024-05-14 21:54:31 -0500

    initial commit

---
+++ src/main/java/tutoriales/rabbitmq/spring/TutorialRabbitmqSpringApplication.java
@@ -0,0 +1,13 @@
+package tutoriales.rabbitmq.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialRabbitmqSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialRabbitmqSpringApplication.class, args);
+    }
+
+}
